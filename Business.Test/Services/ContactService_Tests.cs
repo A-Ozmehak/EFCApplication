5efa051b30cb5a48a5cf71b08d5fff6b1397b63a
Anna <nakupanda89@gmail.com>
2024-02-09 17:12:24 +0100

    changed all tests that returns a contact or true to inMemoryDatabase tests

--- Business.Test/Services/ContactService_Tests.cs
+++ Business.Test/Services/ContactService_Tests.cs
@@ -23,68 +23,22 @@ public class ContactService_Tests
     public void CreateContact_ShouldCreateAndSaveContact_ReturnContact()
     {
         // Arrange
-        var contactDto = new ContactDto
-        {
-            FirstName = "Test",
-            LastName = "User",
-            Email = "test.user@example.com",
-            PhoneNumber = "1234567890",
-            StreetName = "Test Street",
-            StreetNumber = "1",
-            PostalCode = "12345",
-            City = "Test City"
-        };
+        IContactRepository contactRepository = new ContactRepository(_context);
+        IAddressRepository addressRepository = new AddressRepository(_context);
+        IPhoneNumberRepository phoneNumberRepository = new PhoneNumberRepository(_context);
+        IContactService contactService = new ContactService(contactRepository, addressRepository, phoneNumberRepository);
 
-        var contactEntity = new ContactEntity
-        {
-            Id = 1,
-            FirstName = contactDto.FirstName,
-            LastName = contactDto.LastName,
-            Email = contactDto.Email,
-            PhoneNumber = new PhoneNumberEntity { PhoneNumber = contactDto.PhoneNumber },
-            Address = new AddressEntity
-            {
-                StreetName = contactDto.StreetName,
-                StreetNumber = contactDto.StreetNumber,
-                PostalCode = contactDto.PostalCode,
-                City = contactDto.City
-            }
-        };
-
-        var mockContactRepository = new Mock<IContactRepository>();
-        mockContactRepository.Setup(repo => repo.GetOne(It.IsAny<Expression<Func<ContactEntity, bool>>>()))
-            .Returns((ContactEntity)null);
-        mockContactRepository.Setup(repo => repo.Create(It.IsAny<ContactEntity>()))
-            .Returns(contactEntity);
-
-        var mockAddressRepository = new Mock<IAddressRepository>();
-        mockAddressRepository.Setup(repo => repo.GetOne(It.IsAny<Expression<Func<AddressEntity, bool>>>()))
-            .Returns((AddressEntity)null);
-        mockAddressRepository.Setup(repo => repo.Create(It.IsAny<AddressEntity>()))
-            .Returns(contactEntity.Address);
-
-        var mockPhoneNumberRepository = new Mock<IPhoneNumberRepository>();
-        mockPhoneNumberRepository.Setup(repo => repo.GetOne(It.IsAny<Expression<Func<PhoneNumberEntity, bool>>>()))
-            .Returns((PhoneNumberEntity)null);
-        mockPhoneNumberRepository.Setup(repo => repo.Create(It.IsAny<PhoneNumberEntity>()))
-            .Returns(contactEntity.PhoneNumber);
-
-        var contactService = new ContactService(mockContactRepository.Object, mockAddressRepository.Object, mockPhoneNumberRepository.Object);
+        var contactDto = new ContactDto { FirstName = "Anna", LastName = "Ozmehak", Email = "anna.ozmehak@gmail.com", PhoneNumber = "0793555635", StreetName = "Gustaf dalensgatan", StreetNumber = "24", PostalCode = "41724", City = "Goteborg" };
 
         // Act
-        var returnedContactDto = contactService.CreateContact(contactEntity);
+        var result = contactService.CreateContact(contactDto);
 
         // Assert
-        Assert.NotNull(returnedContactDto);
-        Assert.Equal(contactEntity.Id, returnedContactDto.Id);
-        Assert.Equal(contactEntity.FirstName, returnedContactDto.FirstName);
-        Assert.Equal(contactEntity.LastName, returnedContactDto.LastName);
-        Assert.Equal(contactEntity.Email, returnedContactDto.Email);
-        Assert.Equal(contactEntity.PhoneNumber.PhoneNumber, returnedContactDto.PhoneNumber);
-        Assert.Equal(contactEntity.Address.StreetName, returnedContactDto.StreetName);
-        Assert.Equal(contactEntity.Address.StreetNumber, returnedContactDto.StreetNumber);
-        Assert.Equal(contactEntity.Address.PostalCode, returnedContactDto.PostalCode);
-        Assert.Equal(contactEntity.Address.City, returnedContactDto.City);
+        Assert.NotNull(result);
+        var createdContact = _context.Contacts.FirstOrDefault(c => c.FirstName == contactDto.FirstName);
+        Assert.NotNull(createdContact);
+        Assert.Equal(contactDto.Email, createdContact.Email);
+        Assert.Equal(contactDto.PhoneNumber, createdContact.PhoneNumber.PhoneNumber);
     }
 
     [Fact]
@@ -106,30 +60,28 @@ public class ContactService_Tests
     }
 
     [Fact]
-    public void GetAll_ShouldGetAllContacts_ReturnIEnumerableOfTypeContacts()
+    public void GetAll_ShouldGetAllContacts_ReturnContacts()
     {
         // Arrange
-        var mockContactRepository = new Mock<IContactRepository>();
-        var mockAddressRepository = new Mock<IAddressRepository>();
-        var mockPhoneNumberRepository = new Mock<IPhoneNumberRepository>();
-        IContactService contactService = new ContactService(mockContactRepository.Object, mockAddressRepository.Object, mockPhoneNumberRepository.Object);
+        var firstContact = new ContactEntity { FirstName = "Anna", LastName = "Ozmehak", Email = "anna.ozmehak@gmail.com", PhoneNumber = new PhoneNumberEntity { PhoneNumber = "0793555635" },  Address = new AddressEntity { StreetName = "Gustaf dalensgatan", StreetNumber = "24", PostalCode = "41724", City = "Goteborg" } };
+        var secondContact = new ContactEntity { FirstName = "Calle", LastName = "Ozmehak", Email = "ozmehak.calle@gmail.com", PhoneNumber = new PhoneNumberEntity { PhoneNumber = "0793555634" }, Address = new AddressEntity { StreetName = "Gustaf dalensgatan", StreetNumber = "24", PostalCode = "41724", City = "Goteborg" } };
+        _context.Contacts.AddRange(firstContact, secondContact);
+        _context.SaveChanges();
 
-        var contacts = new List<ContactEntity>
-        {
-            new ContactEntity { Id = 1, FirstName = "Test", LastName = "User", Email = "test.user@example.com", Address = new AddressEntity { StreetName = "Gustaf dalensgatan", StreetNumber = "24", PostalCode = "12345", City = "Goteborg" }, PhoneNumber = new PhoneNumberEntity { PhoneNumber = "1234567894" }},
-            new ContactEntity { Id = 2, FirstName = "Test1", LastName = "User1", Email = "test1.user@example.com", Address = new AddressEntity { StreetName = "Test Street 1", StreetNumber = "123", PostalCode = "12345", City = "Test City 1" }, PhoneNumber = new PhoneNumberEntity { PhoneNumber = "1234567890" }},
-        };
-
-        mockContactRepository.Setup(x => x.GetAll()).Returns(contacts);
+        IContactRepository contactRepository = new ContactRepository(_context);
+        IAddressRepository addressRepository = new AddressRepository(_context);
+        IPhoneNumberRepository phoneNumberRepository = new PhoneNumberRepository(_context);
+        var contactService = new ContactService(contactRepository, addressRepository, phoneNumberRepository);
 
         // Act
-        var result = contactService.GetAll();
+        var result = contactService.GetAll().ToList();
 
         // Assert
-        Assert.Equal(2, result.Count());
-        Assert.Contains(result, r => r.FirstName == "Test" && r.LastName == "User" && r.StreetName == "Gustaf dalensgatan" && r.PhoneNumber == "1234567894");
-        Assert.Contains(result, r => r.FirstName == "Test1" && r.LastName == "User1" && r.StreetName == "Test Street 1" && r.PhoneNumber == "1234567890");
+        Assert.Equal(2, result.Count);
+        Assert.Contains(result, r => r.FirstName == firstContact.FirstName && r.Email == firstContact.Email);
+        Assert.Contains(result, r => r.FirstName == secondContact.FirstName && r.Email == secondContact.Email);
     }
+
 
     [Fact]
     public void GetAll_ShouldGetNoContacts_ReturnEmptyIEnumerable()
@@ -152,27 +104,28 @@ public class ContactService_Tests
     }
 
     [Fact]
-    public void GetOne_ShouldGetOneContactById_ReturnOneContact()
+    public void GetOne_IfContactExists_ReturnOneContact()
     {
         // Arrange
-        var mockContactRepository = new Mock<IContactRepository>();
-        var mockAddressRepository = new Mock<IAddressRepository>();
-        var mockPhoneNumberRepository = new Mock<IPhoneNumberRepository>();
-        IContactService contactService = new ContactService(mockContactRepository.Object, mockAddressRepository.Object, mockPhoneNumberRepository.Object);
+        var contact = new ContactEntity { FirstName = "Anna", LastName = "Ozmehak", Email = "anna.ozmehak@gmail.com", PhoneNumber = new PhoneNumberEntity { PhoneNumber = "0793555635" }, Address = new AddressEntity { StreetName = "Gustaf dalensgatan", StreetNumber = "24", PostalCode = "41724", City = "Goteborg" } };
+        _context.Contacts.Add(contact);
+        _context.SaveChanges();
 
-        var contactEntity = new ContactEntity { Id = 1, FirstName = "Test", LastName = "User", Email = "test.user@example.com", Address = new AddressEntity { StreetName = "Test Street", StreetNumber = "123", PostalCode = "12345", City = "Test City" }, PhoneNumber = new PhoneNumberEntity { PhoneNumber = "1234567890" } };
+        IContactRepository contactRepository = new ContactRepository(_context);
+        IAddressRepository addressRepository = new AddressRepository(_context);
+        IPhoneNumberRepository phoneNumberRepository = new PhoneNumberRepository(_context);
+        var contactService = new ContactService(contactRepository, addressRepository, phoneNumberRepository);
 
-        mockContactRepository.Setup(x => x.GetOneById(It.IsAny<Expression<Func<ContactEntity, bool>>>()))
-            .Returns(contactEntity);
+        var contactDto = new ContactDto { Id = contact.Id };
 
         // Act
-        var result = contactService.GetOne(contactEntity);
+        var result = contactService.GetOne(contactDto);
 
         // Assert
         Assert.NotNull(result);
-        Assert.Equal(contactEntity.FirstName, result.FirstName);
-        Assert.Equal(contactEntity.LastName, result.LastName);
-        Assert.Equal(contactEntity.Email, result.Email);
+        Assert.Equal(contact.FirstName, result.FirstName);
+        Assert.Equal(contact.Email, result.Email);
+        Assert.Equal(contact.LastName, result.LastName);
     }
 
     [Fact]
@@ -197,27 +150,22 @@ public class ContactService_Tests
     }
 
     [Fact]
-    public void Update_ShouldUpdateContact_ReturnUpdatedContactEntity()
+    public void Update_ShouldUpdateContact_ReturnUpdated()
     {
         // Arrange
-        var mockContactRepository = new Mock<IContactRepository>();
-        var mockAddressRepository = new Mock<IAddressRepository>();
-        var mockPhoneNumberRepository = new Mock<IPhoneNumberRepository>();
-        IContactService contactService = new ContactService(mockContactRepository.Object, mockAddressRepository.Object, mockPhoneNumberRepository.Object);
+        var contact = new ContactEntity { FirstName = "Anna", LastName = "Ozmehak", Email = "anna.ozmehak@gmail.com", PhoneNumber = new PhoneNumberEntity { PhoneNumber = "0793555635" }, Address = new AddressEntity { StreetName = "Gustaf dalensgatan", StreetNumber = "24", PostalCode = "41724", City = "Goteborg" } };
+        _context.Contacts.Add(contact);
+        _context.SaveChanges();
 
-        var contactEntity = new ContactEntity { FirstName = "Anna", LastName = "Ozmehak", Email = "anna.ozmehak@gmail.com", Address = new AddressEntity { StreetName = "Gustaf dalensgatan", StreetNumber = "24", PostalCode = "41724", City = "Goteborg" }, PhoneNumber = new PhoneNumberEntity { PhoneNumber = "0793555635" } };
-        var contactDto = new ContactDto { FirstName = "Calle", LastName = "Ozmehak", Email = "ozmehak.calle@gmail.com", StreetName = "Gustaf dalensgatan", StreetNumber = "24", PostalCode = "41724", City = "Goteborg", PhoneNumber = "0735687334" };
+        IContactRepository contactRepository = new ContactRepository(_context);
+        IAddressRepository addressRepository = new AddressRepository(_context);
+        IPhoneNumberRepository phoneNumberRepository = new PhoneNumberRepository(_context);
+        IContactService contactService = new ContactService(contactRepository, addressRepository, phoneNumberRepository);
 
-        mockContactRepository.Setup(x => x.GetOne(It.IsAny<Expression<Func<ContactEntity, bool>>>())).Returns(contactEntity);
-        mockContactRepository.Setup(x => x.Update(It.IsAny<ContactEntity>())).Callback<ContactEntity>(c => contactEntity = c);
-
+        var updatedContactDto = new ContactDto { FirstName = "Calle", LastName = "Ozmehak", Email = "ozmehak.calle@gmail.com", StreetName = "Gustaf dalensgatan", StreetNumber = "24", PostalCode = "41724", City = "Goteborg", PhoneNumber = "0793555635" };
+        
         // Act
-        var result = contactService.Update(contactDto);
-
-        // Assert
-        Assert.Equal(contactDto.FirstName, contactEntity.FirstName);
-        Assert.Equal(contactDto.Email, contactEntity.Email);
-        Assert.Equal(contactDto.PhoneNumber, contactEntity.PhoneNumber.PhoneNumber);
+        var result = contactService.Update(updatedContactDto);
     }
 
     [Fact]
@@ -242,24 +190,24 @@ public class ContactService_Tests
     }
 
     [Fact]
-    public void Remove_ShouldRemoveContactByEmail_ReturnTrue()
+    public void Remove_ShouldRemoveContact_ReturnTrue()
     {
         // Arrange
-        var mockContactRepository = new Mock<IContactRepository>();
-        var mockAddressRepository = new Mock<IAddressRepository>();
-        var mockPhoneNumberRepository = new Mock<IPhoneNumberRepository>();
-        IContactService contactService = new ContactService(mockContactRepository.Object, mockAddressRepository.Object, mockPhoneNumberRepository.Object);
+        var contact = new ContactEntity { FirstName = "Anna", LastName = "Ozmehak", Email = "anna.ozmehak@gmail.com", PhoneNumber = new PhoneNumberEntity { PhoneNumber = "0793555635" }, Address = new AddressEntity { StreetName = "Gustaf dalensgatan", StreetNumber = "24", PostalCode = "41724", City = "Goteborg" } };
+        _context.Contacts.Add(contact);
+        _context.SaveChanges();
 
-        var contactEntity = new ContactEntity { Id = 1, FirstName = "Test", LastName = "User", Email = "test.user@example.com", Address = new AddressEntity { StreetName = "Test Street", StreetNumber = "123", PostalCode = "12345", City = "Test City" }, PhoneNumber = new PhoneNumberEntity { PhoneNumber = "1234567890" } };
-
-        mockContactRepository.Setup(x => x.GetOne(It.IsAny<Expression<Func<ContactEntity, bool>>>())).Returns(contactEntity);
-        mockContactRepository.Setup(x => x.Delete(It.Is<Expression<Func<ContactEntity, bool>>>(predicate => predicate.Compile()(contactEntity)))).Returns(true);
+        IContactRepository contactRepository = new ContactRepository(_context);
+        IAddressRepository addressRepository = new AddressRepository(_context);
+        IPhoneNumberRepository phoneNumberRepository = new PhoneNumberRepository(_context);
+        IContactService contactService = new ContactService(contactRepository, addressRepository, phoneNumberRepository);
 
         // Act
-        var result = contactService.Remove(contactEntity.Email);
+        var result = contactService.Remove(contact.Email);
 
         // Assert
         Assert.True(result);
+        Assert.False(_context.Contacts.Any(c => c.Id == contact.Id));
     }
 
     [Fact]
